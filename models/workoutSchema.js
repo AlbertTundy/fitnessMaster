de44a95070b1ad5006a78befdb2e49a3eb98ce27
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-03-15 16:04:05 -0600

    finished the workoutSchema page

---
+++ models/workoutSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+
+const workoutSchema = new mongoose.Schema({
+    day: {
+        type: Date,
+        default: ()=> new Date(),
+
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: "You must enter a type of exercise."
+        },
+        name: {
+            type: String,
+            required: "What's the name of the exercise?"
+        },
+        duration: {
+            type: Number,
+            required: "How long are you lifting this for?"
+        },
+        weight: {
+            type: Number,
+            required: "How much weight did you lift?"
+        },
+        reps: {
+            type: Number,
+            required: "How many reps did you do?"
+        },
+        sets: {
+            type: Number,
+            required: "How many sets did you do?"
+        }
+    }]
+})
+const Workout = mongoose.model("Workout", workoutSchema)
+module.exports = Workout
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = 3000;
+const app = express();
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
