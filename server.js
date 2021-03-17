1c9987aac759b2acf5a0530ee2043775dd7793a4
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-03-16 22:42:54 -0600

    fixed a single spelling error. Hope that's the bug.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://localhost/Workout",
+  process.env.MONGODB_URI || "mongodb://localhost/Workout",
 {
   useNewUrlParser: true,
   useFindAndModify: false,
